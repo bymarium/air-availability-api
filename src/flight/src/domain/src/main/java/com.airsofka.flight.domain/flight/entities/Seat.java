29fae38753839827e04fcc45653eb9ee11899903
Marty Parada <martin.p1131@gmail.com>
2025-03-08 20:43:53 -0500

    feat(feature/flight): add entity Seat to Flight

--- src/flight/src/domain/src/main/java/com.airsofka.flight.domain/flight/entities/Seat.java
+++ src/flight/src/domain/src/main/java/com.airsofka.flight.domain/flight/entities/Seat.java
@@ -8,5 +8,47 @@ import com.airsofka.flight.domain.flight.values.SeatNumber;
 import com.airsofka.shared.domain.generic.Entity;
 
 public class Seat extends Entity<SeatId> {
-    
+    private SeatNumber seatNumber;
+    private SeatClass seatClass;
+    private IsAvailable isAvailable;
+    private PriceSeat priceSeat;
+
+    public Seat(SeatId identity, SeatNumber seatNumber, SeatClass seatClass, IsAvailable isAvailable, PriceSeat priceSeat) {
+        super(identity);
+        this.seatNumber = seatNumber;
+        this.seatClass = seatClass;
+        this.isAvailable = isAvailable;
+        this.priceSeat = priceSeat;
+    }
+    public SeatNumber getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(SeatNumber seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public SeatClass getSeatClass() {
+        return seatClass;
+    }
+
+    public void setSeatClass(SeatClass seatClass) {
+        this.seatClass = seatClass;
+    }
+
+    public IsAvailable getIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(IsAvailable isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public PriceSeat getPriceSeat() {
+        return priceSeat;
+    }
+
+    public void setPriceSeat(PriceSeat priceSeat) {
+        this.priceSeat = priceSeat;
+    }
 }
