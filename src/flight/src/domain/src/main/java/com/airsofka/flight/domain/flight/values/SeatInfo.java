b1bba09b0c93e325682274d6bec497126e9108f4
MartyPR <martin.p1131@gmail.com>
2025-03-11 23:07:08 -0500

    refact(feature/flight_admin): delete primitive data

--- src/flight/src/domain/src/main/java/com/airsofka/flight/domain/flight/values/SeatInfo.java
+++ src/flight/src/domain/src/main/java/com/airsofka/flight/domain/flight/values/SeatInfo.java
@@ -2,7 +2,7 @@ package com.airsofka.flight.domain.flight.values;
 
 public class SeatInfo {
     private final String seatClass;
-    private final double price;
+    private final Double price;
 
     public SeatInfo(String seatClass, double price) {
         this.seatClass = seatClass;
